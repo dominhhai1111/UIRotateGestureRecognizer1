736be28d7df62d239a735d151504a827ea226eb3
dominhhai1096 <dominhhai1096@gmail.com>
2015-10-26 10:37:49 +0700

    Initial Commit

---
+++ GestureRecognizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestureRecognizer
+//
+//  Created by Do Minh Hai on 10/26/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestureRecognizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureRecognizer
+//
+//  Created by Do Minh Hai on 10/26/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
